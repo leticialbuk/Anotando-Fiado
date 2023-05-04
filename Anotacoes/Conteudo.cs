c440513f0de91e107d8ebb7da1f3825dfc933c9c
leticialbuk <leticia.albuk@gmail.com>
2023-05-03 23:18:13 -0300

    utilizando tecnicas de classes do curso OOP

--- Anotacoes/Conteudo.cs
+++ Anotacoes/Conteudo.cs
@@ -11,11 +11,8 @@ namespace Fiados
         }
 
         public string Nome { get; set; }
-
         public int Valor { get; set; }
-
         public int Data { get; set; }
-
         public string Produto { get; set; }
 
         public override string ToString()
